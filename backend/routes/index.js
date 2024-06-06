88de41e3def28a8cf0443cd5570ab0335d56acb1
ivan <ivanchantf@gmail.com>
2024-06-06 13:16:22 +0800

    first commit

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,6 @@
 const router=require("express").Router();
 const apiRouter=require("./apiRoutes");
 router.use("/api",apiRouter);
-
+router.use("/",(req,res)=>{res.json({"startingpage":"Server is running, please use /api for retrieving data"})})
 
 module.exports=router;
